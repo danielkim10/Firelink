e647ef0e26e780ac140d87d6333dbb47ce8f1ad9
Daniel Kim <daniel.k.0710@gmail.com>
2021-02-27 19:11:22 -0500

    routes

--- backend/index.js
+++ backend/index.js
@@ -9,14 +9,15 @@ const { mongoose } = require('./db.js');
 require('./authentication/passport');
 
 var gameController = require('./controllers/game.controller');
-var matchController = require('./controllers/match.controller');
-var roleController = require('./controllers/role.controller');
-var teamController = require('./controllers/team.controller');
-var tournamentController = require('./controllers/tournament.controller');
-var userController = require('./controllers/user.controller');
 var inviteController = require('./controllers/invite.controller');
+var matchController = require('./controllers/match.controller');
 var notificationController = require('./controllers/notification.controller');
 var rankController = require('./controllers/rank.controller');
+var roleController = require('./controllers/role.controller');
+var teamController = require('./controllers/team.controller');
+var teamMemberController = require('./controllers/teamMember.controller');
+var tournamentController = require('./controllers/tournament.controller');
+var userController = require('./controllers/user.controller');
 var routesApi = require('./authentication/index');
 
 var app = express();
@@ -27,14 +28,15 @@ app.use(passport.initialize());
 
 app.listen(3000, () => console.log('Server started at port : 3000'));
 app.use('/games', gameController);
-app.use('/matches', matchController);
-app.use('/roles', roleController);
-app.use('/teams', teamController);
-app.use('/tournaments', tournamentController);
-app.use('/users', userController);
 app.use('/invites', inviteController);
+app.use('/matches', matchController);
 app.use('/notifications', notificationController);
 app.use('/ranks', rankController);
+app.use('/roles', roleController);
+app.use('/teams', teamController);
+app.use('/teamMembers', teamMemberController);
+app.use('/tournaments', tournamentController);
+app.use('/users', userController);
 app.use('/auth', routesApi);
 
 app.use((err, req, res, next) => {
